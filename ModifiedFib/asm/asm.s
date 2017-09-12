761247e1d367bb075548aa4b2d04ee35fa8b892a
Nathan Lowe <techwiz96@gmail.com>
2017-09-12 13:27:49 -0400

    Finnish assembly for Project 0

--- ModifiedFib/asm/asm.s
+++ ModifiedFib/asm/asm.s
@@ -1,34 +1,47 @@
         NAME    main
-        
-        PUBLIC  __iar_program_start
-        
-        SECTION .intvec : CODE (2)
-        THUMB
-        
-__iar_program_start
-        B       main
-
-        
+        PUBLIC  main
         SECTION .text : CODE (2)
-        THUMB
 
-main:   ldr     r7, =results
-        ldr     r7, [r7]
+        DATA
+res:    DS32 25
+
+        // Reg Map:
+        // * r0: a
+        // * r1: b
+        // * r2: c
+        // * r3: tmp
+        //
+        // * r4: res buff base
+        // * r5: i
+
+        THUMB
+main:   ldr     r4, =res
+        eor     r5, r5, r5
+
         eor     r0, r0, r0
+        str     r0, [r4, r5]
+        add     r5, r5, #4
         
-        
-        eor     r0, r0, r0
         mov     r1, #1
+        str     r1, [r4, r5]
+        add     r5, r5, #4
+        
         mov     r2, #1
-        mov     r3, #2
-        
-        
-        B       _halt
+        str     r2, [r4, r5]
+        add     r5, r5, #4
+
+loop:
+        add     r3, r2, r1
+        add     r3, r3, r0
+        str     r3, [r4, r5]
+        add     r5, r5, #4
+        cmp     r5, #(25 *4)
+        beq     _halt
+        mov     r0, r1
+        mov     r1, r2
+        mov     r2, r3
+        b       loop
         
 _halt:  B      _halt
-
-        SECTION .data : DATA(2)
-        DATA
-results:    DC32 0
 
         END
