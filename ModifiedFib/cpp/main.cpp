606905ebbb25a81a725c4151d33b65bb8a5f9f4e
Nathan Lowe <techwiz96@gmail.com>
2017-09-12 14:43:49 -0400

    style fixes for cpp

--- ModifiedFib/cpp/main.cpp
+++ ModifiedFib/cpp/main.cpp
@@ -4,8 +4,8 @@ uint32_t sequence[25];
 
 int main()
 {
-    sequence[0] = static_cast<uint32_t>(0);
-    sequence[1] = static_cast<uint32_t>(1);
+    sequence[0] = 0;
+    sequence[1] = 1;
     sequence[2] = sequence[1];
     
     for(uint8_t i = 3; i < 25; i++)
